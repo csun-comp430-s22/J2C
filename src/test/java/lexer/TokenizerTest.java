a950e8260439489eb14a18e6040295a5d57b609e
ibtehaz <ibtehaz.utsay@gmail.com>
2022-03-02 19:48:36 -0800

    tests are working

--- src/test/java/lexer/TokenizerTest.java
+++ src/test/java/lexer/TokenizerTest.java
@@ -1,20 +1,36 @@
 package lexer;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TokenizerTest {
-    public static void testEmptyString() throws TokenizerException {
+    @Test
+    public void testEmptyString() throws TokenizerException {
         Tokenizer tokenizer = new Tokenizer("");
         List<Token> tokens = tokenizer.tokenize();
-        assert(tokens.size() == 0);
+        assertEquals(0, tokens.size());
+        //assertTrue(tokens.size() == 0);
     }
-    public static void testOnlyWhiteSpace() throws TokenizerException {
+    @Test
+    public void testOnlyWhiteSpace() throws TokenizerException {
         Tokenizer tokenizer = new Tokenizer("   ");
         List<Token> tokens = tokenizer.tokenize();
-        assert(tokens.size() == 0);
+        assertEquals(0, tokens.size());
+        //assertTrue(tokens.size() == 0);
     }
-    public static void main(String[] args) throws TokenizerException {
-        testOnlyWhiteSpace();
-        testEmptyString();
+    @Test
+    public void testTrueByItself() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("true");
+        List<Token> tokens = tokenizer.tokenize();
+        assertEquals(1, tokens.size());
+        Token trueToken = tokens.get(0);
+        assertTrue(trueToken instanceof TrueToken);
+    }
+    // public static void main(String[] args) throws TokenizerException {
+    //     testOnlyWhiteSpace();
+    //     testEmptyString();
 
-    }
+    // }
 }
