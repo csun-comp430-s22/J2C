2d60d407b44563de2be277f39d3b1aadaac69edb
ibtehaz <ibtehaz.utsay@gmail.com>
2022-03-06 05:54:10 -0800

    modify test methods

--- src/test/java/lexer/TokenizerTest.java
+++ src/test/java/lexer/TokenizerTest.java
@@ -1,36 +1,40 @@
 package lexer;
+
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class TokenizerTest {
-    @Test
-    public void testEmptyString() throws TokenizerException {
-        Tokenizer tokenizer = new Tokenizer("");
-        List<Token> tokens = tokenizer.tokenize();
-        assertEquals(0, tokens.size());
-        //assertTrue(tokens.size() == 0);
+    public void assertTokenizes(final String input,
+                                final Token[] expected) {
+        try {
+            final Tokenizer tokenizer = new Tokenizer(input);
+            final List<Token> received = tokenizer.tokenize();
+            assertArrayEquals(expected,
+                              received.toArray(new Token[received.size()]));
+        } catch (final TokenizerException e) {
+            fail("Tokenizer threw exception");
+        }
     }
+    
     @Test
-    public void testOnlyWhiteSpace() throws TokenizerException {
-        Tokenizer tokenizer = new Tokenizer("   ");
-        List<Token> tokens = tokenizer.tokenize();
-        assertEquals(0, tokens.size());
-        //assertTrue(tokens.size() == 0);
+    public void testEmptyString() {
+        assertTokenizes("", new Token[0]);
     }
+
+    @Test
+    public void testOnlyWhitespace() throws TokenizerException {
+        assertTokenizes("    ", new Token[0]);
+    }
+
     @Test
     public void testTrueByItself() throws TokenizerException {
-        Tokenizer tokenizer = new Tokenizer("true");
-        List<Token> tokens = tokenizer.tokenize();
-        assertEquals(1, tokens.size());
-        Token trueToken = tokens.get(0);
-        assertTrue(trueToken instanceof TrueToken);
+        assertTokenizes("true",
+                        new Token[] { new TrueToken() });
     }
-    // public static void main(String[] args) throws TokenizerException {
-    //     testOnlyWhiteSpace();
-    //     testEmptyString();
-
-    // }
 }
